90e663548befc504d5a59ba39cab1d4e06cbd80c
BaldinoGab12 <50639940+BaldinoGab12@users.noreply.github.com>
2019-06-14 17:39:12 +0200

    Classe "EProdotto" funzionante.

---
+++ EProdotto.php
@@ -0,0 +1,67 @@
+<?php
+
+class EProdotto
+{
+    protected $Nome;
+    protected $IDProdotto;
+    protected $Prezzo;
+    protected $Descrizione;
+    protected $Ingredienti;
+    protected $Biologico;
+    protected $Categoria;
+
+
+    public function __construct(String $Nome, int $IDProdotto, float $Prezzo, String $Descrizione, String $Ingredienti, bool $Biologico, String $Categoria)
+
+    {
+
+        $this->Nome = $Nome;
+        $this->IDProdotto = $IDProdotto;
+        $this->Prezzo = $Prezzo;
+        $this->Descrizione = $Descrizione;
+        $this->Ingredienti = $Ingredienti;
+        $this->Biologico = $Biologico;
+        $this->Categoria = $Categoria;
+    }
+
+    public function getNome() : String {return $this->Nome;}
+
+    public function setNome(String $Nome) : void {$this->Nome = $Nome;}
+
+    public function getIDProdotto() : int {return $this->IDProdotto;}
+
+    public function setIDProdotto(int $IDProdotto) : void {$this->IDProdotto = $IDProdotto;}
+
+    public function getPrezzo() : float {return $this->Prezzo;}
+
+    public function setPrezzo(float $Prezzo) : void {$this->Prezzo = $Prezzo;}
+
+    public function getDescrizione() : String {return $this->Descrizione;}
+
+    public function setDescrizione(String $Descrizione) : void {$this->Descrizione = $Descrizione;}
+
+    public function getIngredienti() : String {return $this->Ingredienti;}
+
+    public function setIngredienti(String $Ingredienti) : void {$this->Ingredienti = $Ingredienti;}
+
+    public function getBiologico() : String {
+
+        if ($this->Biologico == 1){
+            return "Si";
+        }
+        else {
+            return "No";
+        }
+    }
+
+    public function setBiologico(bool $Biologico) : void {$this->Biologico = $Biologico;}
+
+    public function getCategoria() : String {return $this->Categoria;}
+
+    public function setCategoria(String $Categoria) : void {$this->Categoria = $Categoria;}
+
+    public function toString() : String {
+
+        return $this->getNome()."\n".$this->getIDProdotto()."\n".$this->getPrezzo()."\n".$this->getDescrizione()."\n".$this->getIngredienti()."\n".$this->getBiologico()."\n".$this->getCategoria();
+    }
+}
