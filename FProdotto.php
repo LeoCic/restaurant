55a2980275ef69fb0515fe043f23c72b1b3ec29c
BaldinoGab12 <50639940+BaldinoGab12@users.noreply.github.com>
2019-07-01 18:56:30 +0200

    Creata "FProdotto". Terminata, da testare.

--- FProdotto.php
+++ FProdotto.php
@@ -48,7 +48,7 @@ abstract class FProdotto
         $Disponibilita = $prodotto->getDisponibilita();
 
         $conn = FDataBase::Connect();
-        $sql = "INSERT INTO Prodotto (`Nome`, `Prezzo`, `Descrizione`, `Ingredienti`, `Biologico`, `Categoria`, `GradoAlcolico`, `Gassato`, `Disponibilita`) VALUES('" . addslashes("$Nome") . "' , '$Prezzo' , '" . addslashes("$Descrizione") . addslashes("$Ingredienti") . "' , '$Biologico' , '" . addslashes("$Categoria") . "' , '$GradoAlcolico' , '$Gassato' , '$Disponibilita')";
+        $sql = "INSERT INTO Prodotto (`Nome`, `Prezzo`, `Descrizione`, `Ingredienti`, `Biologico`, `Categoria`, `GradoAlcolico`, `Gassato`, `Disponibilita`) VALUES('" . addslashes("$Nome") . "' , '" . addslashes("$Prezzo") ."' , '" . addslashes("$Descrizione") . "' , '" . addslashes("$Ingredienti") . "' , '" . addslashes("$Biologico") . "' , '" . addslashes("$Categoria") . "' , '" . addslashes("$GradoAlcolico") . "' , '" . addslashes("$Gassato") . "' , '" . addslashes("$Disponibilita") ."')";
         $riss = $conn->query($sql);
         if (is_bool($riss))
             return 0;
@@ -71,7 +71,7 @@ abstract class FProdotto
         $Integrale = $prodotto->getIntegrale();
 
         $conn = FDataBase::Connect();
-        $sql = "INSERT INTO Prodotto (`Nome`, `Prezzo`, `Descrizione`, `Ingredienti`, `Biologico`, `Categoria`, `Congelato`, `Vegano`, `Glutine`, `Integrale`) VALUES('" . addslashes("$Nome") . "' , '$Prezzo' , '" . addslashes("$Descrizione") . addslashes("$Ingredienti") . "' , '$Biologico' , '" . addslashes("$Categoria") . "' , '$Congelato' , '$Vegano' , '$Glutine' , '$Integrale')";
+        $sql = "INSERT INTO Prodotto (`Nome`, `Prezzo`, `Descrizione`, `Ingredienti`, `Biologico`, `Categoria`, `Congelato`, `Vegano`, `Glutine`, `Integrale`) VALUES('" . addslashes("$Nome") . "' , '" . addslashes("$Prezzo") ."' , '" . addslashes("$Descrizione") . "' , '" . addslashes("$Ingredienti") . "' , '" . addslashes("$Biologico") . "' , '" . addslashes("$Categoria") . "' , '" . addslashes("$Congelato") . "' , '" . addslashes("$Vegano") . "' , '" . addslashes("$Glutine") . "' , '" . addslashes("$Integrale") ."')";
         $riss = $conn->query($sql);
         if (is_bool($riss))
             return 0;
@@ -80,9 +80,63 @@ abstract class FProdotto
 
     }
 
+    public static function update(EBevanda $prodotto) : bool
+    {
 
+        $Nome = $prodotto->getNome();
+        $IDProdotto = $prodotto->getIDProdotto();
+        $Prezzo = $prodotto->getPrezzo();
+        $Descrizione = $prodotto->getDescrizione();
+        $Ingredienti = $prodotto->getIngredienti();
+        $Biologico = $prodotto->getBiologico();
+        $Categoria = $prodotto->getCategoria();
+        $GradoAlcolico = $prodotto->getGradoAlcolico();
+        $Gassato = $prodotto->getGassato();
+        $Disponibilita = $prodotto->getDisponibilita();
 
+        $conn = FDataBase::Connect();
+        $sql =" UPDATE Prodotto SET Nome = '" . addslashes($Nome) . "' , Prezzo = '" . addslashes($Prezzo) . "' , Descrizione = '" . addslashes($Descrizione) . "' , Ingredienti = '" . addslashes($Ingredienti) . "' , Biologico = '" . addslashes($Biologico) . "' , Categoria = '" . addslashes($Categoria) . "' , GradoAlcolico = '" . addslashes($GradoAlcolico) . "' , Gassato = '" . addslashes($Gassato) . "' , Disponibilita = '" . addslashes($Disponibilita) . "' WHERE IDProdotto = '$IDProdotto' " ;
+        $riss = $conn->query($sql);
+        if (is_bool($riss) )
+            return 0;
+        else if(is_object($riss))
+            return 1;
 
+    }
 
+    public static function update1(ECibo $prodotto) : bool
+    {
 
+        $Nome = $prodotto->getNome();
+        $IDProdotto = $prodotto->getIDProdotto();
+        $Prezzo = $prodotto->getPrezzo();
+        $Descrizione = $prodotto->getDescrizione();
+        $Ingredienti = $prodotto->getIngredienti();
+        $Biologico = $prodotto->getBiologico();
+        $Categoria = $prodotto->getCategoria();
+        $Congelato = $prodotto->getCongelato();
+        $Vegano = $prodotto->getVegano();
+        $Glutine = $prodotto->getGlutine();
+        $Integrale = $prodotto->getIntegrale();
+
+        $conn = FDataBase::Connect();
+        $sql =" UPDATE Prodotto SET Nome = '" . addslashes($Nome) . "' , Prezzo = '" . addslashes($Prezzo) . "' , Descrizione = '" . addslashes($Descrizione) . "' , Ingredienti = '" . addslashes($Ingredienti) . "' , Biologico = '" . addslashes($Biologico) . "' , Categoria = '" . addslashes($Categoria) . "' , Congelato = '" . addslashes($Congelato) . "' , Vegano = '" . addslashes($Vegano) . "' , Glutine = '" . addslashes($Glutine) . "' , Integrale = '" . addslashes($Integrale) . "' WHERE IDProdotto = '$IDProdotto' " ;
+        $riss = $conn->query($sql);
+        if (is_bool($riss) )
+            return 0;
+        else if(is_object($riss))
+            return 1;
+
+    }
+
+    public static function delete(String $id) : bool
+    {
+        $conn = FDataBase::Connect();
+        $sql ="DELETE FROM Prodotto WHERE IDProdotto = '$id'";
+        $riss = $conn->query($sql);
+        if (is_bool($riss) )
+            return 0;
+        else if(is_object($riss))
+            return 1;
+    }
 }
