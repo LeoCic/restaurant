ac63af46e6aa423bd51ddd9ca0f9134c0cc21be7
BaldinoGab12 <50639940+BaldinoGab12@users.noreply.github.com>
2019-07-11 10:36:33 +0200

    Creata classe Indici.

---
+++ Indice.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'Foundation/FOrdine.php';
+require_once 'Foundation/FLuogo.php';
+require_once 'Foundation/FGiudizio.php';
+require_once 'Foundation/FProdotto.php';
+require_once 'Foundation/FRistorante.php';
+require_once 'Foundation/FUtente.php';
+require_once 'Foundation/FDatabase.php';
+
+require_once 'Model/EOrdine.php';
+require_once 'Model/ELuogo.php';
+require_once 'Model/EGiudizio.php';
+require_once 'Model/EProdotto.php';
+require_once 'Model/ERistorante.php';
+require_once 'Model/EUtente.php';
+require_once 'Model/EBevanda.php';
+require_once 'Model/ECibo.php';
