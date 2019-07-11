505a96a25fa4fa9c38365485a5a4962d61b70ec1
giuscorr <50442000+giuscorr@users.noreply.github.com>
2019-07-11 10:40:39 +0200

    create le cartelle Model e Foundation
    Signed-off-by: giuscorr <50442000+giuscorr@users.noreply.github.com>

--- Model/EUtente.php
+++ Model/EUtente.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'ELuogo.php';
+require_once '../Indice.php';
 
 class EUtente
 {
--- config.inc.php
+++ config.inc.php
@@ -1,6 +1,3 @@
 <?php 
 $host= 'localhost'; $database= 'restaurant'; $username= 'root'; $password= '';
-
-
-
 ?>
